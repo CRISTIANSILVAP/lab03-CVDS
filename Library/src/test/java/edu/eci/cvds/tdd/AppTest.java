5ddc93a751bed2c28a72cfe548a23fab5ff63fec
CRISTIANSILVAP <cristian.sperilla@mail.escuelaing.edu.co>
2025-02-11 20:47:40 -0500

    Add files via upload

---
+++ Library/src/test/java/edu/eci/cvds/tdd/AppTest.java
@@ -0,0 +1,12 @@
+package edu.eci.cvds.tdd;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+    @Test
+    void testApp() {
+        assertTrue(true); // Solo una prueba básica
+    }
+}
